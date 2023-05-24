b5f9dabb3cc90be46ba89832ae90289a3eb80b52
Jony Azevedo <azevedo1safe2@gmail.com>
2023-05-24 16:30:41 -0300

    Limite de exbição de registro para 10.

--- assets/php/read.php
+++ assets/php/read.php
@@ -1,7 +1,7 @@
 <?php
 include 'conect.php';
 
-$sql = "SELECT * FROM clientes";
+$sql = "SELECT * FROM clientes ORDER BY id ASC LIMIT 10";
 $conbd = $pdo->prepare($sql);
 $conbd->execute(); 
 $clientes = $conbd->fetchAll(PDO::FETCH_ASSOC);
